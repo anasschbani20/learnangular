c4e82d7c29b83a387128429b9ca935a3704edbe2
anass chbani <a.chbani@piman-group.fr>
2022-11-13 16:58:19 +0100

    Loop: ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,14 @@ export class AppComponent {
   calledPhone = '';
   checkingNumber = 0;
   monthNumber = 1;
+  users = [
+    { name: 'Anass', age: 25 },
+    { name: 'Ahmed', age: 30 },
+    { name: 'Mohamed', age: 35 },
+    { name: 'Ali', age: 40 },
+    { name: 'Youssef', age: 45 },
+    { name: 'Hassan', age: 50 },
+  ]
 
   getRandomImage() {
     this.itemImageUrl = this.images[Math.floor(Math.random() * this.images.length)];
